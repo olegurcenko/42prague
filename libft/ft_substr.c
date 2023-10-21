025183278ef868fd7fad4193b34fe12417f0987c
olegurcenko <olegurcenko4@gmail.com>
2023-10-21 20:14:41 +0200

    created rep

---
+++ libft/ft_substr.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oleg <oleg@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 17:23:46 by oleg              #+#    #+#             */
+/*   Updated: 2023/10/21 18:39:15 by oleg             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+static size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i + 1);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*result;
+	size_t		i;
+
+	i = 0;
+	result = malloc((len + 1) * sizeof(char));
+	if ((result == NULL) || (start > ft_strlen(s)))
+	{
+		return (NULL);
+	}
+	while (s[start] != 0 && i != len)
+	{
+		result[i] = s[start];
+		i++;
+		start ++;
+	}
+	result[i] = 0;
+	return (result);
+}
+
+//int main()
+//{
+//    char *b = ft_substr("hello how are you?", 3, 10);
+
+//    if (b != NULL)
+//    {
+//        printf("%s", b);
+//        free(b); // Free the allocated memory
+//    }
+//    else
+//    {
+//        printf("Error: Unable to allocate memory for substring.\n");
+//    }
+
+//    return (0);
+//}
---
+++ libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oleg <oleg@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 17:29:15 by oleg              #+#    #+#             */
+/*   Updated: 2023/10/21 18:36:53 by oleg             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strlen(char *str);
+int		ft_atoi(char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
